f32e3eea7bbe9a90b865b7f243816ec25fe32bbf
C342 <mbuzurin2024@elam.co.uk>
2025-03-17 14:45:59 +0000

    h

--- TreeMender/Assets/Scripts/SaveController.cs
+++ TreeMender/Assets/Scripts/SaveController.cs
@@ -1,37 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEditor.Tilemaps;
 using UnityEngine;
-using UnityEngine.U2D;
 
 public class SaveController : MonoBehaviour
 {
     private string saveLocation;
-    private InventoryController inventoryController;
 
     void Start()
     {
         saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
-        inventoryController = FindObjectOfType<InventoryController>();
-
-        LoadGame();
     }
 
     public void SaveGame()
     {
-        playerPosition = gameObject.FindObjectWithTag("Player").transform.position;
-        mapBoundary = FindObjectOfType<CinemachineConfiner>().m_BoundingShape2D.gameObject.name;
-    }
-
-    File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
-}
-
-public void LoadGame()
-{
-    if (File.Exists(saveLocation))
-    {
-        SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
-
-        GameObject.FindObjectWithTag("Player").transform.position = saveData.playerPosition;
+        SaveData saveData = new SaveData()
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundary = FindObjectOfType<CinemachineConfiner>
+        };
     }
 }

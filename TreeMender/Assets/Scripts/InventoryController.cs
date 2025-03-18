268a12fdedd1d9d2dccaec090e4cc88e4629ef6e
C342 <mbuzurin2024@elam.co.uk>
2025-03-18 15:07:52 +0000

    h

--- TreeMender/Assets/Scripts/InventoryController.cs
+++ TreeMender/Assets/Scripts/InventoryController.cs
@@ -24,20 +24,4 @@ public class InventoryController : MonoBehaviour
             }
         }
     }
-    public bool AddItem(GameObject itemPrefab)
-    {
-        foreach (Transform slotTransform in inventoryPanel.transform)
-        {
-            Slot slot = GetComponent<Slot>();
-            if (slot != null && slot.currentItem == null)
-            {
-                GameObject newItem = Instantiate(itemPrefab, slotTransform);
-                newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-                slot.currentItem = newItem;
-                return true;
-            }
-        }
-        Debug.Log("Inventory is full!");
-        return false;
-    }
 }

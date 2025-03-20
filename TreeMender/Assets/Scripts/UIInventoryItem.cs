be09d13c388f80cb3c5d92c3385829c399d77e3a
C342 <mbuzurin2024@elam.co.uk>
2025-03-20 10:38:38 +0000

    h

--- TreeMender/Assets/Scripts/UIInventoryItem.cs
+++ TreeMender/Assets/Scripts/UIInventoryItem.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIInventoryItem : MonoBehaviour
@@ -17,7 +18,70 @@ public class UIInventoryItem : MonoBehaviour
     private Image borderImage;
 
     public event Action<UIInventoryItem> OnItemClicked,
-         OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag;
+         OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
+        OnRightMouseBtnClick;
 
     private bool empty = true;
+
+    public void Awake()
+    {
+        ResetData();
+        Deselect();
+    }
+
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity + "";
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    public void OnBeginDrag()
+    {
+        if (empty)
+        {
+            return;
+            OnItemBeginDrag?.Invoke(this);
+        }
+    }
+
+    public void OnDrop()
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(BaseEventData Data)
+    {
+        PointerEventData pointerData = (PointerEventData)Data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
+    }
 }

d2ddc7d5aedf74afe1af0c0b707f5a5ceca66248
C342 <mbuzurin2024@elam.co.uk>
2025-03-25 10:39:06 +0000

    h

--- TreeMender/Assets/Scripts/UIInventoryPage.cs
+++ TreeMender/Assets/Scripts/UIInventoryPage.cs
@@ -1,39 +1,50 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Jobs;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Jobs;
 
-namespace Inventory.UI
-{
-    public class UIInventoryPage : MonoBehaviour
-    {
-        [SerializeField]
-        private UIInventoryItem itemPrefab;
+//namespace Inventory
 
-        [SerializeField]
-        private RectTransform contentPanel;
+//{
+//    public class UIInventoryPage : MonoBehaviour
+//    {
+//        [SerializeField]
+//        private UIInventoryItem itemPrefab;
 
-        List<UIInventoryItem> listofUIItems = new List<UIInventoryItem>();
+//        [SerializeField]
+//        private RectTransform contentPanel;
 
-        public void InitializeInventoryUI(int inventorysize)
-        {
-            for (int i = 0; i < inventorysize; i++)
-            {
-                UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
-                uiItem.transform.SetParent(contentPanel);
-                listofUIItems.Add(uiItem);
-            }
-        }
-    }
+//        List<UIInventoryItem> listofUIItems = new List<UIInventoryItem>();
 
-    public void Show()
-    {
-        gameObject.SetActive(true);
-    }
+//        public void InitializeInventoryUI(int inventorysize)
+//        {
+//            for (int i = 0; i < inventorysize; i++)
+//            {
+//                UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+//                uiItem.transform.SetParent(contentPanel);
+//                listofUIItems.Add(uiItem);
+//            }
+//        }
 
-    public void Hide()
-    {
-        gameObject.SetActive(false);
-    }
-}
+//        internal void Hide()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        internal void Show()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public void Show()
+//    {
+//        gameObject.SetActive(true);
+//    }
+
+//    public void Hide()
+//    {
+//        gameObject.SetActive(false);
+//    }
+//}

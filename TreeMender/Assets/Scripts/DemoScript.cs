16c9466c1ad7289b676e839a1f17ee2649462a49
C342 <mbuzurin2024@elam.co.uk>
2025-03-27 11:33:34 +0000

    h

--- TreeMender/Assets/Scripts/DemoScript.cs
+++ TreeMender/Assets/Scripts/DemoScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DemoScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InventoryManager inventoryManager;
+    public Item[] itemsToPickup;
 
-    // Update is called once per frame
-    void Update()
+    public void PickUpItem(int id)
     {
-        
+        inventoryManager.AddItem(itemsToPickup[id]);
     }
 }

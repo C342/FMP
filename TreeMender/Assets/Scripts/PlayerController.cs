40e82d5c9030037d4c76aad6e7b46c2ba0f24210
C342 <mbuzurin2024@elam.co.uk>
2025-03-31 15:35:26 +0100

    Update PlayerController.cs

--- TreeMender/Assets/Scripts/PlayerController.cs
+++ TreeMender/Assets/Scripts/PlayerController.cs
@@ -6,46 +6,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private string currentState;
 
     private Rigidbody2D rb;
     private Animator animator;
 
     private float speedX;
     private float speedY;
-    private bool isFacingRight = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        animator.Play("Idle");
+    }
+
+    void ChangeAnimationState(string newState)
+    {
+
     }
 
     void Update()
     {
-        
+
         speedX = Input.GetAxisRaw("Horizontal");
         speedY = Input.GetAxisRaw("Vertical");
 
-        
+
         animator.SetFloat("xVelocity", Mathf.Abs(speedX));
-        animator.SetFloat("yVelocity", Mathf.Abs(speedY));
-
-        FlipSprite();
-    }
-
-    void FixedUpdate()
-    {
-        rb.velocity = new Vector2(speedX * moveSpeed, speedY * moveSpeed);
-    }
-
-    void FlipSprite()
-    {
-        if ((isFacingRight && speedX < 0) || (!isFacingRight && speedX > 0))
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 ls = transform.localScale;
-            ls.x *= -1f;
-            transform.localScale = ls;
-        }
     }
 }

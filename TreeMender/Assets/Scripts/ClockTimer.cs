697f628d1369cc04e7ecebff036e394caafa6834
C342 <mbuzurin2024@elam.co.uk>
2025-04-25 15:10:47 +0100

    h

--- TreeMender/Assets/Scripts/ClockTimer.cs
+++ TreeMender/Assets/Scripts/ClockTimer.cs
@@ -22,7 +22,7 @@ public class ClockTimer : MonoBehaviour
 
         if (minutes > 15)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
         }
     }
 }

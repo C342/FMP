c9ccac3399fe1caf9116aa28df1f4bc57314647c
C342 <mbuzurin2024@elam.co.uk>
2025-04-29 11:42:39 +0100

    h

---
+++ TreeMender/Assets/Scripts/AcornSpawner.cs
@@ -0,0 +1,22 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class AcornSpawner : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+    public GameObject acornPrefab;
+
+    private void Start()
+    {
+        SpawnAcorns();
+    }
+
+    void SpawnAcorns()
+    {
+        for (int i = 0; i < spawnLocations.Length; i++)
+        {
+            GameObject newAcorn = Instantiate<GameObject>(acornPrefab);
+            newAcorn.transform.position = spawnLocations[i].position; 
+        }
+    }
+}

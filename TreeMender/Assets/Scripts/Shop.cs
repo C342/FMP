d8610257a85eeee9126105877af8563c71f485f5
C342 <mbuzurin2024@elam.co.uk>
2025-04-30 12:34:09 +0100

    cs

---
+++ TreeMender/Assets/Scripts/Shop.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public int Acorns;
+
+    public int WaterBucket;
+    public int MWaterCan;
+    public int LWaterCan;
+
+    public TextMeshProUGUI cText;
+    public TextMeshProUGUI BucketText;
+    public TextMeshProUGUI MWaterCanText;
+    public TextMeshProUGUI LWaterCanText;
+
+    private void Start()
+    {
+        Acorns = 1000;
+        cText.text = Acorns.ToString();
+    }
+
+    public void BuyWaterBucket()
+    {
+        if (Acorns >= 50)
+        {
+            Acorns -= 50;
+            cText.text = Acorns.ToString();
+
+            WaterBucket += 1;
+            BucketText.text = WaterBucket.ToString();
+        }
+        else
+        {
+            print("Not enough acorns!");
+        }
+    }
+
+    public void BuyMWaterCan()
+    {
+        if (Acorns >= 75)
+        {
+            Acorns -= 75;
+            cText.text = Acorns.ToString();
+
+            MWaterCan += 1;
+            MWaterCanText.text = MWaterCan.ToString();
+        }
+        else
+        {
+            print("Not enough acorns!");
+        }
+    }
+    public void BuyLWaterCan()
+    {
+        if (Acorns >= 100)
+        {
+            Acorns -= 100;
+            cText.text = Acorns.ToString();
+
+            LWaterCan += 1;
+            LWaterCanText.text = LWaterCan.ToString();
+        }
+        else
+        {
+            print("Not enough acorns!");
+        }
+    }
+}
